2a3e7945fb5b338c6b5594dcd193ae98ffdd3a9a
Rafael <rafaelrodrigues_@outlook.com>
2020-06-07 15:47:08 -0300

    Adcionado comentários e organizado o código

--- script.js
+++ script.js
@@ -1,18 +1,26 @@
-var filterCaracter = function(event) {
-    return ((event.charCode >= 48 && event.charCode <= 57) || (event.charCode == 44 || event.charCode == 46))
-}
+// Permite apenas a digitação de números, ponto e vírgula
+let filterCaracter = function (event) {
+  return (
+    (event.charCode >= 48 && event.charCode <= 57) ||
+    event.charCode == 44 ||
+    event.charCode == 46
+  );
+};
 
+// Verifica se os inputs estão preenchidos
+// Se não estiver remove a classe numbers e adciona a classe erro
+// Se estiverem preenchidos, realiza a soma e preenche o resultado na input result
 function somar() {
-    n1 = document.querySelector("#number-one");
-    n2 = document.querySelector("#number-two");
-    result = document.querySelector("#result");
-    if (n1.value == '') {
-        n1.classList.add('erro');
-    } else if (n2.value == ''){
-        n2.classList.add('erro');
-    } else {
-        result.value = parseFloat(n1.value) + parseFloat(n2.value);
-        n1.classList.remove('erro');
-        n2.classList.remove('erro');
-    }
+  n1 = document.querySelector("#number-one");
+  n2 = document.querySelector("#number-two");
+  result = document.querySelector("#result");
+  if (n1.value == "") {
+    n1.classList.add("erro");
+  } else if (n2.value == "") {
+    n2.classList.add("erro");
+  } else {
+    result.value = parseFloat(n1.value) + parseFloat(n2.value);
+    n1.classList.remove("erro");
+    n2.classList.remove("erro");
+  }
 }
